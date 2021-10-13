8d9d565252ff2ba0c9d27a4a1e3b8c669997ba33
LauraMaddaluno <lauralondon.4@gmail.com>
2021-10-13 13:58:17 +0100

    crete navbar with a link to a new post and brand name

--- spec/features/user_can_submit_posts_spec.rb
+++ spec/features/user_can_submit_posts_spec.rb
@@ -9,3 +9,11 @@ RSpec.feature "Timeline", type: :feature do
     expect(page).to have_content("Hello, world!")
   end
 end
+
+# RSpec.feature "_header.html.erb", type: :view do
+#   scenario "displays navbar" do
+#     visit "/posts"
+    
+#     expect(page).to have_css("nav class")
+#   end
+# end
