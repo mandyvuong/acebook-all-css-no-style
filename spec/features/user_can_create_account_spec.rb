1f2dcf212bf3082dc5177b2ba28756310d1bb2f3
jake <jakebs540@gmail.com>
2021-10-14 21:19:23 +0100

    Create account test working

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -1,13 +1,12 @@
 require 'rails_helper'
 RSpec.feature 'User can create an account' do
   scenario 'Create an account' do
-    visit '/'
-    # click_button 'sign up'
-    fill_in('first name', with: 'CSS')
-    fill_in('last name', with: 'Style')
-    fill_in('email', with: 'email@example.com')
-    fill_in('password', with: 'supersecret123')
-    click_button 'Create account'    
+    visit '/users/new'
+    fill_in('First name', with: 'CSS')
+    fill_in('Last name', with: 'Style')
+    fill_in('Email', with: 'email@example.com')
+    fill_in('Password', with: 'supersecret123')
+    click_button 'Create Account'    
     expect(current_path).to eq '/users/photo'
   end
 end
