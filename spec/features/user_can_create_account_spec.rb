eefdf4f97ebc01775fd31a77dbf69f53bf0e6ff7
jake <jakebs540@gmail.com>
2021-10-14 18:53:41 +0100

    first name & last name, now stored on the database

--- spec/features/user_can_create_account_spec.rb
+++ spec/features/user_can_create_account_spec.rb
@@ -2,9 +2,9 @@ require 'rails_helper'
 RSpec.feature 'User can create an account' do
   scenario 'Create an account' do
     visit '/'
-    click_button 'sign up'
+    # click_button 'sign up'
     fill_in('first name', with: 'CSS')
-    fill_in('second name', with: 'Style')
+    fill_in('last name', with: 'Style')
     fill_in('email', with: 'email@example.com')
     fill_in('password', with: 'supersecret123')
     click_button 'Create account'    
