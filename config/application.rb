d3a1a4821cff7f3629399195d13c131a99294579
Luke Taylor <99ltaylor@gmail.com>
2021-10-20 11:35:49 +0100

    if statement config/application.rb

--- config/application.rb
+++ config/application.rb
@@ -1,8 +1,9 @@
 require 'rails/all'
 require_relative 'boot'
 Bundler.require(*Rails.groups)
-Dotenv::Railtie.load
-
+if ['development', 'test'].include? ENV['RAILS_ENV']
+  Dotenv::Railtie.load
+end
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
